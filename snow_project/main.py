63205e2e6379866ec268137ae259e6295bec4256
nageshkumar13 <nageshkumar1925@gmail.com>
2026-02-09 18:08:18 +0530

    updated

--- snow_project/main.py
+++ snow_project/main.py
@@ -1,7 +1,7 @@
 from snowflake_client import fetch_query
 
 def main():
-    query = "SELECT * FROM YOUR_TABLE LIMIT 10"
+    query = "SELECT * FROM users LIMIT 10"
     results = fetch_query(query)
 
     for row in results:
